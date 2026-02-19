ba04d4ef9b4387fda1c16dfede745c2da66917b7
Josh Black-Star <josh@blackstar.dev>
2026-02-19 15:03:53 -0800

    relay 0.1.3
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/relay.rb
+++ Formula/relay.rb
@@ -1,8 +1,8 @@
 class Relay < Formula
     desc "Short description of Relay"
     homepage "https://github.com/jdblackstar/relay"
-    url "https://github.com/jdblackstar/relay/archive/refs/tags/v0.1.2.tar.gz"
-    sha256 "45ed2f545b8da815494fd337027dbc5a2cb1fb96cda52ed0a9fdb41d61248b1c"
+    url "https://github.com/jdblackstar/relay/archive/refs/tags/v0.1.3.tar.gz"
+    sha256 "3434261fc62d12691b4c2ca00978fb23cf7208219e119a28ee6fd82bf358e189"
     license "MIT"
   
     depends_on "rust" => :build
